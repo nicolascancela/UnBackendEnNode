d21b4347e759d0313d0f4ef8f97e213dc75fe6ce
Nico <nicolasecancelag@gmail.com>
2023-04-12 19:25:25 -0300

    Added health check

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-
-app.get('/', (req: Request, res: Response) => { res.send(`El server se ejecuta correctamente en el puerto: ${PORT}`) });
+app.get('/health', (req: Request, res: Response) => { res.status(200).send(`El server esta OK`)});
+app.get('/', (req: Request, res: Response) => { res.send(`El server se ejecuta correctamente en el puerto: ${PORT}`); });
 app.use(router);
 app.listen(PORT, () => console.log(`El server se ejecuta correctamente en el puerto: ${PORT}`));
