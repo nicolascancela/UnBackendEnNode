f3f95a839854efbe60c51d7a0231b23e2664afc5
Nico <nicolas.cancela@getronics.com>
2023-04-14 21:30:31 -0300

    Refactor

--- index.ts
+++ index.ts
@@ -6,7 +6,8 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
-app.get('/health', (req: Request, res: Response) => { res.status(200).send(`El server esta OK`) });
-app.get('/', (req: Request, res: Response) => { res.send(`El server se ejecuta correctamente en el puerto: ${PORT}`); });
 app.use(router);
+
+app.get('/health', (req: Request, res: Response) => res.status(200).send('Server is OK!'));
+app.get('/', (req: Request, res: Response) => { res.send(`El server se ejecuta correctamente en el puerto: ${PORT}`); });
 app.listen(PORT, () => console.log(`El server se ejecuta correctamente en el puerto: ${PORT}`));
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,18 +1,13 @@
 import { Request, Response, Router } from "express";
 import { getPeople } from "../service/service";
 import { printPeopleMiddleware, returnPeopleMiddleware, setPeopleMiddleware } from "../midlewares/middleware";
-
 const router = Router();
-
-router.get('/health', (req: Request, res: Response) => res.status(200).send('Server is OK!'));
 
 router.get('/people', setPeopleMiddleware, printPeopleMiddleware, returnPeopleMiddleware);
 
-router.post('/people', function (req: Request, res: Response) {
+router.post('/people', (req: Request, res: Response) => {
     const people = getPeople();
     res.send(people);
 });
-
-
 
 export default router;
