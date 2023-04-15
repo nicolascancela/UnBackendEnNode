bfc88a03fc3fad1209bb3d7486ce64d31d7534ed
Nico <nicolas.cancela@getronics.com>
2023-04-14 21:09:42 -0300

    Fix error Railway

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import express, { Response, Request } from 'express';
 import router from './src/routes/routes';
+require('dotenv').config();
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
-app.get('/health', (req: Request, res: Response) => { res.status(200).send(`El server esta OK`)});
+app.get('/health', (req: Request, res: Response) => { res.status(200).send(`El server esta OK`) });
 app.get('/', (req: Request, res: Response) => { res.send(`El server se ejecuta correctamente en el puerto: ${PORT}`); });
 app.use(router);
 app.listen(PORT, () => console.log(`El server se ejecuta correctamente en el puerto: ${PORT}`));
